a65003766b34e5713ce5b51952640ea45c9fa61f
shailesh1416 <shaileshyadav1416@gmail.com>
2023-05-13 15:55:24 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(cors());
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const session = require('express-session');
