27b6835851df09a9c9e075cc01e21a3142fa7d27
shailesh1416 <shaileshyadav1416@gmail.com>
2023-05-20 10:42:09 +0530

    added feature to get one blog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -43,8 +43,21 @@ const getBlogs = async (req, res) => {
       }
 }
 
+const getOneBlog = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = await Blog.findById(id);
+        // console.log(data)
+        res.json(data);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+      }
+}
+
 module.exports =  {
     uploadBlog,
     blogView,
-    getBlogs
+    getBlogs,
+    getOneBlog
 };
--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {uploadBlog,blogView,getBlogs} = require('../controllers/blogController');
+const {uploadBlog,blogView,getBlogs,getOneBlog} = require('../controllers/blogController');
 const { protectRoute } = require("../auth/protect");
 
 const router = express.Router();
 // blog
 router.get("/",protectRoute, blogView);
 router.get("/all", getBlogs);
+router.get("/:id", getOneBlog);
 
 router.post('/', protectRoute,uploadBlog);
 module.exports = router;
